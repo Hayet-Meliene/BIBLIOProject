8df53ed10cfdd4a1880fc636e68da0abf1c2a783
Meliene Hayet <melienehayet25@gmail.com>
2021-02-25 09:34:47 +0100

    first commit

---
+++ src/biblio/Livre.java
@@ -0,0 +1,24 @@
+package biblio;
+
+public class Livre {
+	private int idlivre;
+	private String titre;
+	public int getIdlivre() {
+		return idlivre;
+	}
+	public void setIdlivre(int idlivre) {
+		this.idlivre = idlivre;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	
+	public void ajouterlivre(Livre l) {
+		
+	}
+
+}
